9b829c836f535eb8c23bf58ad5f427278e7ab5fd
Christian Butcher <chrisb2244@gmail.com>
2023-04-06 19:43:07 +0900

    Add the PPL_Name as an environment variable when tagging. This is necessary to handle libraries with spaces in their names, because the artifact path depends on the library name, not the pipeline name (which has spaces substituted).

--- scripts/YamlGenerator.py
+++ scripts/YamlGenerator.py
@@ -191,7 +191,8 @@ git_tag_stage = {"git_tag": {
     "approval": "success",
     "fetch_materials": "yes",
     "environment_variables": {
-      "GITHUB_RELEASE_TOKEN": "{{SECRET:[secrets.json][github_publishing_token]}}"
+      "GITHUB_RELEASE_TOKEN": "{{SECRET:[secrets.json][github_publishing_token]}}",
+      "PPL_NAME": "#{PPL_Name}"
     },
     # Single job, so no need for jobs entry
     "tasks": git_tag_tasks
