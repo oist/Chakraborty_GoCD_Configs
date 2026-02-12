30d7f0e5ddd71959b32413abab49b90b14d7b62a
Christian Butcher <chrisb2244@gmail.com>
2026-02-12 17:30:46 +0900

    Remove the duplicate pushd function from the GitTools file. Reformat using the Black formatter

--- scripts/FileUtils.py
+++ scripts/FileUtils.py
@@ -1,8 +1,34 @@
 import os
+import contextlib
+import re
+from pathlib import Path
+
+
+@contextlib.contextmanager
+def pushd(new_dir):
+    prev_dir = os.getcwd()
+    os.chdir(new_dir)
+    yield
+    os.chdir(prev_dir)
+
 
 def find_file(filename, top_directory, absolute=True):
-  for dirpath, dirnames, files in os.walk(top_directory):
-    for file in files:
-      if file == filename:
-        return os.path.join(dirpath, file) if absolute else os.path.relpath(os.path.join(dirpath, file), top_directory)
-  return None
+    for dirpath, dirnames, files in os.walk(top_directory):
+        for file in files:
+            if file == filename:
+                return (
+                    os.path.join(dirpath, file)
+                    if absolute
+                    else os.path.relpath(os.path.join(dirpath, file), top_directory)
+                )
+    return None
+
+
+directoryNameMatcher = re.compile(r"^.*:.*/(.*)$")
+
+
+def directoryFromGitRepo(gitRepo, output_dir=None):
+    dest = directoryNameMatcher.match(gitRepo).group(1)
+    if output_dir:
+        dest = os.path.join(Path(output_dir), dest)
+    return dest
--- scripts/GitTools.py
+++ scripts/GitTools.py
@@ -4,46 +4,56 @@ import os
 import shutil
 import stat
 import subprocess
+from FileUtils import pushd
 
-@contextlib.contextmanager
-def pushd(new_dir):
-	prev_dir = os.getcwd()
-	os.chdir(new_dir)
-	yield
-	os.chdir(prev_dir)
 
 def remove_readonly(func, path, excinfo):
-	os.chmod(path, stat.S_IWRITE)
-	func(path)
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
 
 def runCmd(cmd):
-  return subprocess.run(cmd, capture_output=True, text=True, shell=False).stdout
+    return subprocess.run(cmd, capture_output=True, text=True, shell=False).stdout
+
 
 def cloneRepo(gitUrl, destinationDirectory, forceUpdate=False):
-  # Can add --depth 1 but only marginal improvement at the moment...
-  # gitCmd = f'wsl git clone -q  -- {gitUrl}'
-  gitCmd = ['git',  'clone', '-q',  '--', gitUrl, destinationDirectory]
-  cloneResult = subprocess.run(gitCmd, capture_output=True, text=True, shell=False)
-  exists = "already exists and is not an empty directory" in cloneResult.stderr
-  if cloneResult.returncode == 128 and exists:
-    if forceUpdate:
-      print(f"Directory already exists. Deleting and re-cloning repository")
-      shutil.rmtree(destinationDirectory, onerror=remove_readonly)
-      return cloneRepo(gitUrl, destinationDirectory, False) # Don't pass true again, to prevent infinite recursion.
+    # Can add --depth 1 but only marginal improvement at the moment...
+    # gitCmd = f'wsl git clone -q  -- {gitUrl}'
+    gitCmd = ["git", "clone", "-q", "--", gitUrl, destinationDirectory]
+    cloneResult = subprocess.run(gitCmd, capture_output=True, text=True, shell=False)
+    exists = "already exists and is not an empty directory" in cloneResult.stderr
+    if cloneResult.returncode == 128 and exists:
+        if forceUpdate:
+            print(f"Directory already exists. Deleting and re-cloning repository")
+            shutil.rmtree(destinationDirectory, onerror=remove_readonly)
+            return cloneRepo(
+                gitUrl, destinationDirectory, False
+            )  # Don't pass true again, to prevent infinite recursion.
+        else:
+            response = updateRepo(destinationDirectory, True)
+            return (
+                f"Directory already existed at {destinationDirectory}. Updated the repository\n"
+                + response.strip()
+                + "\n"
+            )
+    elif cloneResult.returncode != 0:
+        # Other error, possibly authentication or bad argument
+        raise RuntimeError(
+            f"Error executing command: {gitCmd}\n" + str(cloneResult.stderr)
+        )
     else:
-      response = updateRepo(destinationDirectory, True)
-      return f"Directory already existed at {destinationDirectory}. Updated the repository\n" + response.strip() + "\n"
-  elif cloneResult.returncode != 0:
-    # Other error, possibly authentication or bad argument
-    raise RuntimeError(f"Error executing command: {gitCmd}\n" + str(cloneResult.stderr))
-  else:
-    # Success
-    return f"Cloned {gitUrl} to {destinationDirectory}\n" + cloneResult.stdout.strip() + "\n"
+        # Success
+        return (
+            f"Cloned {gitUrl} to {destinationDirectory}\n"
+            + cloneResult.stdout.strip()
+            + "\n"
+        )
 
-def updateRepo(destinationDirectory, useRemote = True):
-  with pushd(destinationDirectory):
-    if (useRemote):
-      runCmd(["git", "fetch"])
-    response = runCmd(["git", "reset", "--hard", "origin/master"])
-    runCmd(["git", "clean", "-f"])
-    return response.strip()
+
+def updateRepo(destinationDirectory, useRemote=True):
+    with pushd(destinationDirectory):
+        if useRemote:
+            runCmd(["git", "fetch"])
+        response = runCmd(["git", "reset", "--hard", "origin/master"])
+        runCmd(["git", "clean", "-f"])
+        return response.strip()
