e3c593367ab93a2da729f9e7f5882c183406ff4c
Christian Butcher <chrisb2244@gmail.com>
2026-03-03 10:16:59 +0900

    Expand the FPGA pipelines to create necessary links for PPL references, and to provide better insight into the setup during build.

--- scripts/Generate_FPGA_Pipelines.py
+++ scripts/Generate_FPGA_Pipelines.py
@@ -10,9 +10,49 @@ from Constants import (
     fpga_build_task,
     fpga_artifact_path,
     script_fpga_version_task,
+    create_ppl_dir,
+    ls_task,
+    ls_currentDir_task,
 )
 
 cachedMaterials = {}
+
+mklink_fpga_tasks = {
+    "debug": {
+        "exec": {
+            "run_if": "passed",
+            "command": "powershell",
+            "arguments": [
+                "-Command",
+                "New-Item",
+                "-Force",
+                "-ItemType",
+                "Junction",
+                "-Path",
+                "PPLs\\cRIO-9045\\home",  # relative path?
+                "-Target",
+                f'\\"C:\\LabVIEW Sources\\PPLs\\cRIO-9045\\Debug_32\\home\\"',
+            ],
+        }
+    },
+    "release": {
+        "exec": {
+            "run_if": "passed",
+            "command": "powershell",
+            "arguments": [
+                "-Command",
+                "New-Item",
+                "-Force",
+                "-ItemType",
+                "Junction",
+                "-Path",
+                "PPLs\\cRIO-9045\\home",  # relative path?
+                "-Target",
+                f'\\"C:\\LabVIEW Sources\\PPLs\\cRIO-9045\\Release_32\\home\\"',
+            ],
+        }
+    },
+}
 
 
 class PipelineDefinition_FPGA(yaml.YAMLObject):
@@ -45,7 +85,7 @@ class PipelineDefinition_FPGA(yaml.YAMLObject):
                 "LV_VERSION": self.lv_version,
                 "FPGA_TARGET_NAME": self.targetName,
                 "FPGA_BUILDSPEC_NAME": self.buildSpecName,
-                "PROJECT_PATH": f"C:\\LabVIEW Sources\\{gitDirName}\\{self.projectFileName}",
+                "PROJECT_PATH": f"{gitDirName}\\{self.projectFileName}",
                 "VERSION_VI_PATH": self.version_vi_path,
             },
             "materials": materials,
@@ -75,6 +115,10 @@ class PipelineDefinition_FPGA(yaml.YAMLObject):
                                     }
                                 ],
                                 "tasks": [
+                                    create_ppl_dir,
+                                    mklink_fpga_tasks["debug"],
+                                    ls_task,
+                                    ls_currentDir_task,
                                     script_fpga_version_task,
                                     fpga_build_task,
                                 ],
