6f06dd5f72c948a0dc8ee212ddcb39d9c6b9c970
Christian Butcher <chrisb2244@gmail.com>
2026-04-09 11:11:51 +0900

    Set the deployment host default value.

--- scripts/Generate_RT_Pipeline.py
+++ scripts/Generate_RT_Pipeline.py
@@ -141,7 +141,7 @@ class PipelineDefinition_RTapp(yaml.YAMLObject):
                 "BUILD_NUMBER_OFFSET": "540",
             },
             "environment_variables": {
-                "CRIO_HOST": "",  # Must be set when triggering deployment
+                "CRIO_HOST": "crio.chakraborty.lab",  # Default deployment host
                 "CRIO_USER": "admin",  # Default SSH user for cRIO
                 "PACKAGE_SERVER_UPLOAD_USER": "pkgupload",
                 "PACKAGE_SERVER_UPLOAD_KEY": "~/.ssh/id_rsa",
